f373f7dc7679b70640ac765475cd328af45e9333
Alex Cornejo <acornejo@gmail.com>
2016-04-09 19:13:08 -0700

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
-var rucksack = require('rucksack-css')
-var webpack = require('webpack')
-var path = require('path')
+var rucksack = require('rucksack-css');
+var webpack = require('webpack');
+var path = require('path');
+
+var plugins = [];
+var js_loaders = ['babel-loader'];
+
+if (process.env.NODE_ENV == "production") {
+  plugins.push(new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'));
+  plugins.push(new webpack.optimize.DedupePlugin());
+  // plugins.push(new webpack.optimize.UglifyJsPlugin({sourceMap: false}));
+  plugins.push(new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify("production")
+  }));
+} else {
+  plugins.push(new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'));
+  plugins.push(new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify("development")
+  }));
+  js_loaders.unshift('react-hot');
+}
 
 module.exports = {
   context: path.join(__dirname, './client'),
@@ -43,10 +61,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        loaders: [
-          'react-hot',
-          'babel-loader'
-        ]
+        loaders: js_loaders
       },
     ],
   },
@@ -58,12 +73,7 @@ module.exports = {
       autoprefixer: true
     })
   ],
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development') }
-    })
-  ],
+  plugins: plugins,
   devServer: {
     contentBase: './client',
     hot: true
